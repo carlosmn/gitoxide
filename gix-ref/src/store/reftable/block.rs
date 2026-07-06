7e6875c6e0390c1e93015840c576effbe1b66bf8
Carlos Martín Nieto <cmn@dwim.me>
2026-07-06 14:35:08 +0200

    gix-ref: add an implementation for a reftable `Block`
    This is largely metadata about where things go. An iterator is needed to extract
    data from it.

---
+++ gix-ref/src/store/reftable/block.rs
@@ -0,0 +1,115 @@
+use super::blocksource::Source;
+use super::{BlockType, Error, Result};
+
+use bytes::{Buf, Bytes};
+
+const DEFAULT_BLOCK_SIZE: u32 = 4096;
+
+/// A block in the reftable.
+///
+/// Once created, a block is immutable
+pub(crate) struct Block {
+    /// Offset of the block header; nonzero for the first block in a reftable
+    pub(crate) header_off: u32,
+
+    /// Memory block
+    pub(crate) block_data: Bytes,
+    pub(crate) hash_size: u32,
+
+    /* some compression fields for use with log entries come here */
+    /// Restart point data. Restart points are located after the block's record
+    /// data.
+    pub(crate) restart_count: u16,
+    pub(crate) restart_off: u32,
+
+    /// Size of the data in the file. For log blocks, this is the compressed
+    /// size.
+    pub(crate) full_block_size: u32,
+
+    pub(crate) block_type: BlockType,
+}
+
+/// Read a block from the source while making sure we don't run off the end
+///
+/// We cut off at the end of the block rather than returning an error like the
+/// source itself would so we can guess at the block size in [`Block::new()`]
+/// safely.
+fn read_block(source: &dyn Source, off: u64, mut sz: u32) -> Result<Bytes> {
+    let size = source.size();
+    if off >= size {
+        // The C git.git implementation returns 0 here after setting everything
+        // to NULL, indicating success but trying to continue would lead to a
+        // segfault. Here we can simulate that via unreachable!() as we in fact
+        // do not expect the offset to ever be larger than the source.
+        unreachable!();
+    }
+
+    if off.checked_add(sz as u64).ok_or(Error::IoError)? > size {
+        sz = size
+            .checked_sub(off)
+            .and_then(|n| n.try_into().ok())
+            .ok_or(Error::IoError)?;
+    }
+
+    source.read(off, sz)
+}
+
+impl Block {
+    /// Create a new block from the source and given data from the table.
+    pub fn new(
+        source: &dyn Source,
+        offset: u32,
+        header_size: u32,
+        table_block_size: u32,
+        hash_size: u32,
+        want_type: Option<BlockType>,
+    ) -> Result<Self> {
+        // If there is no given table block size we guess at a sensible 4k for
+        // the block and adjust if necessary once we figure out the block size.
+        let guess_block_size = if table_block_size > 0 {
+            table_block_size
+        } else {
+            DEFAULT_BLOCK_SIZE
+        };
+
+        let mut block_data = read_block(source, offset as u64, guess_block_size)?;
+        let block_type: BlockType = block_data[header_size as usize].try_into()?;
+        if want_type.is_some() && want_type != Some(block_type) {
+            return Err(Error::MismatchedBlockType);
+        }
+
+        let block_size = super::get_be24(&block_data[(header_size + 1) as usize..]);
+        if block_size > guess_block_size {
+            block_data = read_block(source, offset as u64, block_size)?;
+        }
+
+        let mut full_block_size = table_block_size;
+        if block_type == BlockType::Log {
+            /* Compression setup happens here  */
+            todo!();
+        } else if full_block_size == 0 {
+            full_block_size = block_size;
+        } else if block_size < full_block_size
+            && block_size < block_data.len() as u32
+            && block_data[block_size as usize] != 0
+        {
+            // If the block is smaller than the full block size, it is
+            // padded (data followed by '\0') or the next block is
+            // unaligned.
+            full_block_size = block_size;
+        }
+
+        let restart_count = (&block_data[(block_size - 2) as usize..]).get_u16();
+        let restart_off = block_size - 2 - (3 * restart_count) as u32;
+
+        Ok(Self {
+            header_off: header_size,
+            block_data,
+            hash_size,
+            restart_count,
+            restart_off,
+            full_block_size,
+            block_type,
+        })
+    }
+}
